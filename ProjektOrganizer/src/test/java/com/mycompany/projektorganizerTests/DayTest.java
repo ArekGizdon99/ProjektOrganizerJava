0f3da0930e10cd8de01ae49aeea2db5a7b575075
ArekGizdon99 <56155506+ArekGizdon99@users.noreply.github.com>
2021-11-27 19:30:03 +0100

    test

--- ProjektOrganizer/src/test/java/com/mycompany/projektorganizerTests/DayTest.java
+++ ProjektOrganizer/src/test/java/com/mycompany/projektorganizerTests/DayTest.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projektorganizer;
+package com.mycompany.projektorganizerTests;
 
+import com.mycompany.projektorganizer.Day;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
--- ProjektOrganizer/src/test/java/com/mycompany/projektorganizerTests/DaysListTest.java
+++ ProjektOrganizer/src/test/java/com/mycompany/projektorganizerTests/DaysListTest.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.mycompany.projektorganizer;
+package com.mycompany.projektorganizerTests;
 
+import com.mycompany.projektorganizer.Day;
+import com.mycompany.projektorganizer.DaysList;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
